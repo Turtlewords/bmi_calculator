e738ad4fa46fade3192134408a1f19e09c785da8
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-04 22:47:34 -0500

    initial js. selecting elements

---
+++ index.js
@@ -0,0 +1,35 @@
+// Elements
+
+const metricRadio = document.querySelector("#metric");
+const imperialRadio = document.querySelector("#imperial");
+const heightMetric = document.querySelector("#height-metric");
+const weighttMetric = document.querySelector("#weight-metric");
+const ftImperial = document.querySelector("ft-imperial");
+const inImperial = document.querySelector("in-imperial");
+const stImperial = document.querySelector("st-imperial");
+const lbsImperial = document.querySelector("lbs-imperial");
+
+const welcome = document.querySelector("#welcome");
+const output = document.querySelector("#output");
+const bmiResult = document.querySelector("#bmi-result");
+const classification = document.querySelector("#classification");
+const range = document.querySelector("#range");
+
+
+// Variables
+
+let isMetric = true;
+let isImperial = false;
+
+
+// Event Listeners
+
+
+
+
+
+
+
+
+
+// Functions
